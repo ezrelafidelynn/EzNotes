db9477f3025db92caffc711ddcca7fb8f18f97bc
ezrelafidelynn <ezre252@gmail.com>
2025-06-28 18:19:16 +0700

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
 
 //local import
 import notesRoutes from "./routes/notesRoutes.js";
@@ -13,17 +14,23 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
+
+
+
+if(process.env.NODE_ENV !== "production") {
+    app.use(
+        cors({
+            origin: ["http://localhost:5173"],
+        }
+        )
+    );
+}
 
 //middleware
 app.use(express.json());
 app.use(rateLimiter);
-app.use(
-    cors({
-        origin: ["http://localhost:5173"],
-    }
-    )
-);
 
 // app.use((req,res,next) =>{
 //     console.log("We just got a new request");
@@ -31,6 +38,15 @@ app.use(
 // })
 
 app.use("/api/notes", notesRoutes);
+
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+
+    app.get("*", (req,res) => {
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 
 
 connectDB().then(() => {
